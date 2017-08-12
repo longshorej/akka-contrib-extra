3df179837293df05f8b3dfc8fa107473a6a7f96a
Christopher Hunt <huntchr@gmail.com>
2017-08-12 16:09:41 +1000

    Provide a non-blocking process implementation
    Utilises NuProcess in order to provide a non-blocking process implementation for Windows, OS X and Linux.

---
+++ src/main/scala/akka/contrib/process/NonBlockingProcess.scala
@@ -0,0 +1,214 @@
+/**
+ * Copyright (C) 2017 Lightbend Inc. <http://www.lightbend.com>
+ */
+
+package akka.contrib.process
+
+import akka.actor.{ Actor, ActorLogging, NoSerializationVerificationNeeded, PoisonPill, Props }
+import akka.stream.scaladsl.{ BroadcastHub, Keep, Sink, Source }
+import akka.util.ByteString
+import java.io.File
+import java.nio.ByteBuffer
+import java.util.concurrent.atomic.AtomicReference
+
+import akka.stream.stage.{ AsyncCallback, GraphStageLogic, GraphStageWithMaterializedValue, OutHandler }
+import akka.stream._
+import akka.{ Done, NotUsed }
+import com.zaxxer.nuprocess.{ NuAbstractProcessHandler, NuProcess, NuProcessBuilder }
+
+import scala.collection.JavaConverters
+import scala.collection.immutable
+import scala.concurrent.{ Future, blocking }
+
+object NonBlockingProcess {
+
+  /**
+   * Sent to the receiver on startup - specifies the streams used for managing input, output and error respectively.
+   * This message should only be received by the parent of the NonBlockingProcess and should not be passed across the
+   * JVM boundary (the publishers are not serializable).
+   *
+   * @param pid the process id
+   * @param stdin a `akka.stream.scaladsl.Sink[ByteString]` for the standard input stream of the process
+   * @param stdout a `akka.stream.scaladsl.Source[ByteString]` for the standard output stream of the process
+   * @param stderr a `akka.stream.scaladsl.Source[ByteString]` for the standard error stream of the process
+   */
+  case class Started(
+    pid: Long,
+    stdin: Sink[ByteString, Future[Done]],
+    stdout: Source[ByteString, NotUsed],
+    stderr: Source[ByteString, NotUsed]) extends NoSerializationVerificationNeeded
+
+  /**
+   * Sent to the receiver after the process has exited.
+   *
+   * @param exitValue the exit value of the process
+   */
+  case class Exited(exitValue: Int)
+
+  /**
+   * Send a request to destroy the process.
+   * On POSIX, this sends a SIGTERM, but implementation is platform specific.
+   */
+  case object Destroy
+
+  /**
+   * Send a request to forcibly destroy the process.
+   * On POSIX, this sends a SIGKILL, but implementation is platform specific.
+   */
+  case object DestroyForcibly
+
+  /**
+   * Create Props for a [[NonBlockingProcess]] actor.
+   *
+   * @param command signifies the program to be executed and its optional arguments
+   * @param workingDir the working directory for the process; default is the current working directory
+   * @param environment the environment for the process; default is `Map.emtpy`
+   * @return Props for a [[NonBlockingProcess]] actor
+   */
+  def props(
+    command: immutable.Seq[String],
+    workingDir: File = new File(System.getProperty("user.dir")),
+    environment: Map[String, String] = Map.empty) =
+    Props(new NonBlockingProcess(command, workingDir, environment))
+
+  private[process] object PublishIfAvailableSideChannel {
+    sealed trait AsyncEvents
+    case class Publish[T](e: T) extends AsyncEvents
+    case class Complete[T](e: Option[T]) extends AsyncEvents
+  }
+
+  private[process] abstract class PublishIfAvailableSideChannel[T] {
+    def publishIfAvailable(e: () => T): Unit
+    def complete(e: Option[T]): Unit
+  }
+
+  /*
+   * The motivation for this type of source is to publish *only* if any downstream
+   * is ready to receive. The assumption is that only one thread will be calling
+   * the publishIfAvailable function of the side channel at any one time.
+   */
+  private[process] class PublishIfAvailable[T]
+      extends GraphStageWithMaterializedValue[SourceShape[T], PublishIfAvailableSideChannel[T]] {
+
+    val out = Outlet[T]("PublishIfAvailable.out")
+    override def shape = SourceShape(out)
+
+    override def createLogicAndMaterializedValue(inheritedAttributes: Attributes): (GraphStageLogic, PublishIfAvailableSideChannel[T]) = {
+      val asyncCallback = new AtomicReference[AsyncCallback[PublishIfAvailableSideChannel.AsyncEvents]]
+      val logic = new GraphStageLogic(shape) {
+
+        override def preStart(): Unit =
+          asyncCallback.set(getAsyncCallback[PublishIfAvailableSideChannel.AsyncEvents] {
+            case PublishIfAvailableSideChannel.Publish(e: T @unchecked) =>
+              push(out, e)
+            case PublishIfAvailableSideChannel.Complete(e: Option[T] @unchecked) =>
+              e.foreach(emit(out, _))
+              completeStage()
+          })
+
+        setHandler(out, new OutHandler {
+          override def onPull(): Unit =
+            ()
+        })
+      }
+
+      logic -> new PublishIfAvailableSideChannel[T] {
+        override def publishIfAvailable(e: () => T): Unit =
+          Option(asyncCallback.get).foreach { ac =>
+            if (logic.isAvailable(out)) ac.invoke(PublishIfAvailableSideChannel.Publish(e()))
+          }
+
+        override def complete(e: Option[T]): Unit =
+          Option(asyncCallback.get).foreach(_.invoke(PublishIfAvailableSideChannel.Complete(e)))
+      }
+    }
+  }
+}
+
+/**
+ * NonBlockingProcess encapsulates an operating system process and its ability to be communicated with via stdio i.e.
+ * stdin, stdout and stderr. The reactive streams for stdio are communicated in a NonBlockingProcess.Started event
+ * upon the actor being established. The parent actor is then subsequently streamed
+ * stdout and stderr events. When the process exists (determined by periodically polling process.isAlive()) then
+ * the process's exit code is communicated to the receiver in a NonBlockingProcess.Exited event.
+ *
+ * All IO is performed in a non-blocking manner. Herein lies the difference between [[BlockingProcess]] and this
+ * actor. As such, many more processes can be managed via this while consuming *much* less memory on your JVM.
+ * Always favour this actor over its blocking counterpart.
+ */
+class NonBlockingProcess(
+  command: immutable.Seq[String],
+  directory: File,
+  environment: Map[String, String])
+    extends Actor with ActorLogging {
+
+  import NonBlockingProcess._
+
+  val process: NuProcess = {
+    import JavaConverters._
+
+    val pb = new NuProcessBuilder(command.asJava)
+
+    pb.environment().putAll(environment.asJava)
+
+    pb.setCwd(directory.toPath)
+
+    val parent = context.parent
+    pb.setProcessListener(new NuAbstractProcessHandler {
+      override def onPreStart(nuProcess: NuProcess): Unit = {
+        // Create our stream based actors away from this one given that we want them to continue
+        // for a small while post the actor dying (it may take a tiny bit longer for the process
+        // to terminate).
+        implicit val stdioMaterializer: ActorMaterializer = ActorMaterializer()(context.system)
+        val stdin =
+          Sink
+            .foreach[ByteString](bytes => nuProcess.writeStdin(bytes.toByteBuffer))
+        val (out, stdout) =
+          Source
+            .fromGraph(new PublishIfAvailable[ByteString])
+            .toMat(BroadcastHub.sink)(Keep.both)
+            .run
+        val (err, stderr) =
+          Source
+            .fromGraph(new PublishIfAvailable[ByteString])
+            .toMat(BroadcastHub.sink)(Keep.both)
+            .run
+        parent ! Started(nuProcess.getPID, stdin, stdout, stderr)
+
+        nuProcess.setProcessHandler(new NuAbstractProcessHandler {
+          override def onStderr(buffer: ByteBuffer, closed: Boolean): Unit =
+            if (!closed)
+              err.publishIfAvailable(() => ByteString.fromByteBuffer(buffer))
+            else
+              err.complete(if (buffer.hasRemaining) Some(ByteString.fromByteBuffer(buffer)) else None)
+
+          override def onExit(exitCode: Int): Unit = {
+            parent ! NonBlockingProcess.Exited(exitCode)
+            self ! PoisonPill
+          }
+
+          override def onStdout(buffer: ByteBuffer, closed: Boolean): Unit =
+            if (!closed)
+              out.publishIfAvailable(() => ByteString.fromByteBuffer(buffer))
+            else
+              out.complete(if (buffer.hasRemaining) Some(ByteString.fromByteBuffer(buffer)) else None)
+        })
+      }
+    })
+
+    log.debug("Process starting: {}", command.headOption.getOrElse("<unknown>"))
+    pb.start()
+  }
+
+  override def receive: Receive = {
+    case Destroy =>
+      log.debug("Received request to destroy the process.")
+      blocking(process.destroy(false))
+    case DestroyForcibly =>
+      log.debug("Received request to forcibly destroy the process.")
+      blocking(process.destroy(true))
+  }
+
+  override def postStop(): Unit =
+    process.destroy(true)
+}
---
+++ src/test/scala/akka/contrib/process/NonBlockingProcessSpec.scala
@@ -0,0 +1,170 @@
+/**
+ * Copyright (C) 2009-2014 Typesafe Inc. <http://www.typesafe.com>
+ */
+
+package akka.contrib.process
+
+import java.io.File
+
+import akka.actor._
+import akka.pattern.ask
+import akka.stream.scaladsl.{ Sink, Source }
+import akka.stream.{ ActorMaterializer, ActorMaterializerSettings }
+import akka.testkit.TestProbe
+import akka.util.{ ByteString, Timeout }
+import org.scalatest.{ BeforeAndAfterAll, Matchers, WordSpec }
+
+import scala.collection.immutable
+import scala.concurrent.Await
+import scala.concurrent.duration.{ Duration, DurationInt }
+
+class NonBlockingProcessSpec extends WordSpec with Matchers with BeforeAndAfterAll {
+
+  implicit val system = ActorSystem("test", testConfig)
+
+  implicit val processCreationTimeout = Timeout(2.seconds)
+
+  "A NonBlockingProcess" should {
+    "read from stdin and write to stdout" in {
+      val command = getClass.getResource("/echo.sh").getFile
+      new File(command).setExecutable(true)
+
+      val streamProbe = TestProbe()
+      val exitProbe = TestProbe()
+      val stdinInput = List("abcd", "1234", "quit")
+      val receiver = system.actorOf(Props(new NonBlockingReceiver(streamProbe.ref, exitProbe.ref, command, stdinInput, 1)), "receiver1")
+      val process = Await.result(receiver.ask(NonBlockingReceiver.Process).mapTo[ActorRef], processCreationTimeout.duration)
+
+      val partiallyReceived =
+        streamProbe.expectMsgPF() {
+          case NonBlockingReceiver.Out("abcd1234") =>
+            false
+          case NonBlockingReceiver.Out("abcd") =>
+            true
+        }
+
+      if (partiallyReceived) {
+        streamProbe.expectMsg(NonBlockingReceiver.Out("1234"))
+      }
+
+      exitProbe.expectMsgPF() {
+        case NonBlockingProcess.Exited(x) => x
+      } shouldEqual 0
+
+      exitProbe.watch(process)
+      exitProbe.expectTerminated(process)
+    }
+
+    "allow a blocking process that is blocked to be destroyed" in {
+      expectDestruction(viaDestroy = true)
+    }
+
+    "allow a blocking process that is blocked to be stopped" in {
+      expectDestruction(viaDestroy = false)
+    }
+
+    "be able to create the reference.conf specified limit of processes" in {
+      val command = getClass.getResource("/echo.sh").getFile
+      new File(command).setExecutable(true)
+
+      val probesAndProcesses = for (seed <- 1 to 100) yield {
+        val streamProbe = TestProbe()
+        val exitProbe = TestProbe()
+        val receiver = system.actorOf(Props(new NonBlockingReceiver(streamProbe.ref, exitProbe.ref, command, List.empty, seed)), "receiver-ref-" + seed)
+        val process = Await.result(receiver.ask(NonBlockingReceiver.Process).mapTo[ActorRef], processCreationTimeout.duration)
+        (streamProbe, exitProbe, process)
+      }
+
+      probesAndProcesses.foreach {
+        case (_, exitProbe, process) =>
+          process ! NonBlockingProcess.Destroy
+          exitProbe.watch(process)
+          exitProbe.expectMsgAnyClassOf(classOf[Terminated], classOf[NonBlockingProcess.Exited])
+      }
+    }
+
+    "detect when a process has exited of its own accord" in {
+      val command = getClass.getResource("/loop.sh").getFile
+      new File(command).setExecutable(true)
+      val nameSeed = scala.concurrent.forkjoin.ThreadLocalRandom.current().nextLong()
+      val streamProbe = TestProbe()
+      val exitProbe = TestProbe()
+      val receiver = system.actorOf(Props(new NonBlockingReceiver(streamProbe.ref, exitProbe.ref, command, List.empty, nameSeed)), "receiver" + nameSeed)
+      val process = Await.result(receiver.ask(NonBlockingReceiver.Process).mapTo[ActorRef], processCreationTimeout.duration)
+
+      exitProbe.watch(process)
+
+      process ! NonBlockingProcess.Destroy
+
+      exitProbe.fishForMessage(5.seconds) {
+        case NonBlockingProcess.Exited(r) => true
+        case _                            => false
+      }
+    }
+  }
+
+  override protected def afterAll(): Unit = {
+    Await.ready(system.terminate(), Duration.Inf)
+  }
+
+  def expectDestruction(viaDestroy: Boolean): Unit = {
+    val command = getClass.getResource("/sleep.sh").getFile
+    new File(command).setExecutable(true)
+    val nameSeed = scala.concurrent.forkjoin.ThreadLocalRandom.current().nextLong()
+    val streamProbe = TestProbe()
+    val exitProbe = TestProbe()
+    val receiver = system.actorOf(Props(new NonBlockingReceiver(streamProbe.ref, exitProbe.ref, command, List.empty, nameSeed)), "receiver" + nameSeed)
+    val process = Await.result(receiver.ask(NonBlockingReceiver.Process).mapTo[ActorRef], processCreationTimeout.duration)
+
+    streamProbe.expectMsg(NonBlockingReceiver.Out("Starting"))
+
+    if (viaDestroy)
+      process ! NonBlockingProcess.Destroy
+    else
+      system.stop(process)
+
+    exitProbe.expectMsgPF(10.seconds) {
+      case NonBlockingProcess.Exited(v) => v
+    } should not be 0
+
+    exitProbe.watch(process)
+    exitProbe.expectTerminated(process, 10.seconds)
+  }
+}
+
+object NonBlockingReceiver {
+  case object Process
+  case class Out(s: String)
+  case class Err(s: String)
+}
+
+class NonBlockingReceiver(streamProbe: ActorRef, exitProbe: ActorRef, command: String, stdinInput: immutable.Seq[String], nameSeed: Long) extends Actor
+    with Stash {
+
+  final implicit val materializer = ActorMaterializer(ActorMaterializerSettings(context.system))
+
+  val process = context.actorOf(NonBlockingProcess.props(List(command)), "process" + nameSeed)
+  import NonBlockingReceiver._
+  import context.dispatcher
+
+  override def receive: Receive = {
+    case Process =>
+      sender() ! process
+
+    case NonBlockingProcess.Started(_, stdin, stdout, stderr) =>
+      stdout
+        .map(element => Out(element.utf8String))
+        .merge(stderr.map(element => Err(element.utf8String)))
+        .runWith(Sink.foreach(streamProbe.tell(_, Actor.noSender)))
+        .onComplete(_ => self ! "flow-complete")
+
+      Source(stdinInput).map(ByteString.apply).runWith(stdin)
+    case "flow-complete" =>
+      unstashAll()
+      context become {
+        case exited: NonBlockingProcess.Exited => exitProbe ! exited
+      }
+    case _ =>
+      stash()
+  }
+}
